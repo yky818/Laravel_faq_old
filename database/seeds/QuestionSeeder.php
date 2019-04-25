60133735ce7f4b9a54c7c65e4c3dc8b26eb5e473
Developer <yky5@njit.edu>
2019-04-25 11:16:37 +0530

    Added Question and user seeder.

--- database/seeds/QuestionSeeder.php
+++ database/seeds/QuestionSeeder.php
@@ -11,6 +11,13 @@ class QuestionSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $users = App\User::all();
+        for ($i = 1; $i <= 16; $i++) {
+            $users->each(function ($user) {
+                $question = factory(\App\Question::class)->make();
+                $question->user()->associate($user);
+                $question->save();
+            });
+        }
     }
 }
